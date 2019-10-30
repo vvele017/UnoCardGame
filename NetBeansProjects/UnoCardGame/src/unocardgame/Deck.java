1b3249141eb02341aa19f34ddf4a0db9abc0abf7
vvele <vvele@Vane-gaming-PC.attlocal.net>
2019-10-29 21:51:17 -0400

    Deals out the cards to players

---
+++ NetBeansProjects/UnoCardGame/src/unocardgame/Deck.java
@@ -0,0 +1,13 @@
+/*
+ *  Vanessa Velez-Santos
+ * October 29,2019  
+
+Class will deal out the cards to players and randomly generate
+which type of card
+ */
+package unocardgame;
+
+public class Deck 
+{
+    
+}
