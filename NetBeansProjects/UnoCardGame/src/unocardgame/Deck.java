678a6495efadf4391b751f128ede83105486a554
vvelezsa <vvelezsa@L340533-1400TS.mdc.edu>
2019-10-30 12:08:27 -0400

    Added clockwise variable to keep track of game's direction

--- NetBeansProjects/UnoCardGame/src/unocardgame/Deck.java
+++ NetBeansProjects/UnoCardGame/src/unocardgame/Deck.java
@@ -4,7 +4,10 @@
 
 Class will deal out the cards to players and generate the deck
 
-A UNO deck consists of 108 cards, of which there are 76 Number cards, 24 Action cards and 8 Wild cards.
+A UNO deck consists of 108 cards, of which there are 
+76 Number cards,
+24 Action cards 
+8 Wild cards.
  */
 package unocardgame;
 
@@ -16,12 +19,15 @@ public class Deck
                       NUM_CARDS     = 76,
                       ACTION_CARDS  = 24,
                       WILD_CARDS    = 8;
+    private boolean clockwise;
     
     private Card[] deck = new Card[DECK_SIZE]; //full deck size for all cards
     
+    //only one deck made everytime a game starts
+    //shuffle and assign deck here
     public Deck()
     {
-        
+        clockwise = true; //game always starts in this direction
     }
     
     
