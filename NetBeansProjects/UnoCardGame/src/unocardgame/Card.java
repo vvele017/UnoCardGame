ce782282dc0b2feca8df16cedf739097acf62224
vvele <vvele@Vane-gaming-PC.attlocal.net>
2019-10-29 21:45:03 -0400

    Added Card and Player classes to keep track of cards to be delt and Players

---
+++ NetBeansProjects/UnoCardGame/src/unocardgame/Card.java
@@ -0,0 +1,13 @@
+/*
+ *  Vanessa Velez-Santos
+ * October 29,2019  
+
+Class to keep track of the type of cards available and their affects
+
+ */
+package unocardgame;
+
+public class Card 
+{
+    
+}
---
+++ NetBeansProjects/UnoCardGame/src/unocardgame/Player.java
@@ -0,0 +1,13 @@
+/*
+ *  Vanessa Velez-Santos
+ * October 29,2019  
+
+Keeps track of player info and number of cards in their hands
+ */
+package unocardgame;
+
+public class Player 
+{
+    private final int HAND_SIZE = 7;
+    private Card[] hand = new Card[HAND_SIZE];
+}
